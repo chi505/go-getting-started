e62a0fc6eed8abe1ca236b1be0a1b530dd331689
Patrick Dixon <patrick@patrickmdixon.com>
2018-03-22 23:47:47 +0000

    ,

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		"Hello World"
+		c.String(http.StatusOK, "Hello World", nil)
 	})
 	//	router.GET("/", func(c *gin.Context) {
 	//		c.HTML(http.StatusOK, "index.tmpl.html", nil)
