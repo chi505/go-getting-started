4031a05cda32ae3d6f4a36a7e2e7f80d6f9eca0a
Patrick Dixon <patrick@patrickmdixon.com>
2018-03-25 03:34:52 +0000

    Find the templates

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	var count = 0
 	router := gin.New()
 	router.Use(gin.Logger())
-	//router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
 	//	router.GET("/", func(c *gin.Context) {
